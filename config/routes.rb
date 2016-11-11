2221cf6f20218684deeb8f3b1b75bbbcc9a3377b
stressedesserts <noel.bacani@gmail.com>
2016-11-10 18:05:12 -0800

    fixed sign out routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users, path: 'auth', controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' } 
 
   devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy'
+    get 'sign_out', :to => 'devise/sessions#destroy'
   end
 
   get 'welcome/index'
