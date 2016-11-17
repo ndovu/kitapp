55fc534ee6f6d103855e10791da6b32381569d47
stressedesserts <noel.bacani@gmail.com>
2016-11-16 22:26:13 -0800

    added carrierwave to account edit, but image is not displaying correctly. will figure out later

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  mount_uploader :image, AvatarUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117042221) do
+ActiveRecord::Schema.define(version: 20161117060149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20161117042221) do
     t.string   "provider"
     t.string   "uid"
     t.integer  "bag_id"
+    t.string   "image"
+    t.string   "avatar"
   end
 
   add_index "users", ["bag_id"], name: "index_users_on_bag_id", using: :btree
